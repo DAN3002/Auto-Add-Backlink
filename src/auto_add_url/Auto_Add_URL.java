848e74aa815e045fd066b9417048921dbab0a4a5
= <=>
2018-04-19 21:55:37 +0700

    Set up  Project

---
+++ src/auto_add_url/Auto_Add_URL.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package auto_add_url;
+
+/**
+ *
+ * @author D.A.N_3002
+ */
+public class Auto_Add_URL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
